9e2f9e5172f4dbe9f27aac9ddfa62d6164c6254b
Ferhat Attar <ferhat.attar@iktos.com>
2023-01-30 16:04:05 +0100

    feat: add initial rdkit instance in provider props

--- src/contexts/RDKitContext.tsx
+++ src/contexts/RDKitContext.tsx
@@ -5,16 +5,20 @@ export interface RDKitContextValue {
   RDKit: RDKitModule | null;
 }
 
+export type RDKitProviderProps = PropsWithChildren<{
+  initialRdkitInstance?: RDKitModule;
+}>;
+
 // force default context to be undefined, to check if package users have wrapped it with the required provider
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const RDKitContext = React.createContext<RDKitContextValue>(undefined as any);
 
-export const RDKitProvider: React.FC<PropsWithChildren> = ({ children }) => {
-  const [RDKit, setRDKit] = useState<RDKitModule | null>(null);
+export const RDKitProvider: React.FC<RDKitProviderProps> = ({ initialRdkitInstance, children }) => {
+  const [RDKit, setRDKit] = useState(initialRdkitInstance ?? null);
 
   useEffect(() => {
     let isProviderMounted = true;
-    if (globalThis.initRDKitModule) {
+    if (!initialRdkitInstance && globalThis.initRDKitModule) {
       globalThis.initRDKitModule().then((loadedRDKit) => {
         if (isProviderMounted) {
           setRDKit(loadedRDKit);
@@ -24,7 +28,7 @@ export const RDKitProvider: React.FC<PropsWithChildren> = ({ children }) => {
     return () => {
       isProviderMounted = false;
     };
-  }, []);
+  }, [initialRdkitInstance]);
 
   return <RDKitContext.Provider value={{ RDKit }}>{children}</RDKitContext.Provider>;
 };
