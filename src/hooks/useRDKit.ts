ba4ffd39a8224a3ee91d1912af02f7210b203e81
RamziWeslati <ramzi.oueslati@ensi-uma.tn>
2023-01-31 13:25:30 +0100

    chore: change organisation (#11)
    See #10

--- src/hooks/useRDKit.ts
+++ src/hooks/useRDKit.ts
@@ -5,7 +5,7 @@ import { RDKitContext } from '../contexts';
 export const useRDKit = () => {
   const loadRDKitContext = useContext(RDKitContext);
   if (!loadRDKitContext) {
-    throw new Error('@iktos/rdkit-provider was used outside of RDKitProvider');
+    throw new Error('@iktos-oss/rdkit-provider was used outside of RDKitProvider');
   }
 
   return loadRDKitContext;
