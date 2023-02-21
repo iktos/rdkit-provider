ebfa7d0938c84242e88c42037ac307385951f6c5
Ramzi Oueslati <ramzi.oueslati@ensi-uma.tn>
2023-02-21 21:19:55 +0100

    feat: add rdkit utility functions to worker/consumer

--- src/worker/utils/molecule.ts
+++ src/worker/utils/molecule.ts
@@ -7,3 +7,43 @@ export const getSvg = (smiles: string, drawingDetails: string) => {
   release_molecule(mol);
   return svg;
 };
+
+export const getMoleculeDetails = (smiles: string) => {
+  const mol = get_molecule(smiles, globalThis.workerRDKit);
+  if (!mol) return null;
+  const details = JSON.parse(mol.get_descriptors());
+  release_molecule(mol);
+  return {
+    numAtoms: details.NumHeavyAtoms,
+    numRings: details.NumRings,
+  };
+};
+
+export const getCanonicalFormForStructure = (structure: string): string | null => {
+  if (isValidSmiles(structure)) return getCanonicalSmiles(structure);
+  return getCanonicalSmarts(structure);
+};
+
+export const isValidSmiles = (smiles: string): boolean => {
+  if (!smiles) return false;
+  const mol = get_molecule(smiles, globalThis.workerRDKit);
+  if (!mol) return false;
+  const isValid = mol.is_valid();
+  release_molecule(mol);
+  return isValid;
+};
+
+const getCanonicalSmiles = (smiles: string): string | null => {
+  const mol = get_molecule(smiles, globalThis.workerRDKit);
+  if (!mol) return null;
+  const cannonicalSmiles = mol.get_smiles();
+  release_molecule(mol);
+  return cannonicalSmiles;
+};
+const getCanonicalSmarts = (smarts: string): string | null => {
+  const mol = get_molecule(smarts, globalThis.workerRDKit);
+  if (!mol) return null;
+  const canoncialSmarts = mol.get_smarts();
+  release_molecule(mol);
+  return canoncialSmarts;
+};
