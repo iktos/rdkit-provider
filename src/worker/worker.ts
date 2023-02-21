586f63d88e923ff314e2f1909d5e11a49795a422
Ramzi Oueslati <ramzi.oueslati@ensi-uma.tn>
2023-02-21 23:10:52 +0100

    ref(worker): type guard worker message type See #30

--- src/worker/worker.ts
+++ src/worker/worker.ts
@@ -1,4 +1,3 @@
-import { RDKitProviderCacheOptions } from '../contexts';
 import { cleanAllCache } from '../utils';
 import { getActionLocalResponseIdentifier, RDKIT_WORKER_ACTIONS, WorkerMessage } from './actions';
 import { initRdkit } from './utils/initRDKit';
@@ -15,35 +14,28 @@ addEventListener('message', async ({ data }: { data: WorkerMessage }) => {
   let responsePayload;
   switch (data.actionType) {
     case RDKIT_WORKER_ACTIONS.INIT_RDKIT_MODULE:
-      // TODO remove as ... once narrowing is implemented
-      await initRdkit({ cache: data.payload?.cache as RDKitProviderCacheOptions });
+      await initRdkit({ cache: data.payload?.cache });
       break;
     case RDKIT_WORKER_ACTIONS.GET_MOLECULE_DETAILS:
-      if (!data.payload) return;
-      responsePayload = getMoleculeDetails(data.payload.smiles as string);
+      responsePayload = getMoleculeDetails(data.payload.smiles);
       break;
     case RDKIT_WORKER_ACTIONS.GET_CANONICAL_FORM_FOR_STRUCTURE:
-      if (!data.payload) return;
-      responsePayload = { canonicalForm: getCanonicalFormForStructure(data.payload.structure as string) };
+      responsePayload = { canonicalForm: getCanonicalFormForStructure(data.payload.structure) };
       break;
     case RDKIT_WORKER_ACTIONS.GET_SVG:
-      if (!data.payload) return;
-      responsePayload = { svg: getSvg(data.payload.smiles as string, data.payload.drawingDetails as string) };
+      responsePayload = { svg: getSvg(data.payload.smiles, data.payload.drawingDetails) };
       break;
     case RDKIT_WORKER_ACTIONS.IS_VALID_SMILES:
-      if (!data.payload) return;
-      responsePayload = { isValid: isValidSmiles(data.payload.smiles as string) };
+      responsePayload = { isValid: isValidSmiles(data.payload.smiles) };
       break;
     case RDKIT_WORKER_ACTIONS.IS_VALID_SMARTS:
-      if (!data.payload) return;
-      responsePayload = { isValid: isValidSmarts(data.payload.smarts as string) };
+      responsePayload = { isValid: isValidSmarts(data.payload.smarts) };
       break;
     case RDKIT_WORKER_ACTIONS.HAS_MATCHING_SUBSTRUCTURE:
-      if (!data.payload) return;
       responsePayload = {
         matching: hasMatchingSubstructure({
-          smiles: data.payload.smiles as string,
-          substructure: data.payload.substructure as string,
+          smiles: data.payload.smiles,
+          substructure: data.payload.substructure,
         }),
       };
       break;
