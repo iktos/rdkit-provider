166fee6f3803f9a6427d3df6a90f812595ffd96f
Ramzi Oueslati <ramzi.oueslati@ensi-uma.tn>
2023-03-14 12:31:52 +0100

    chore(worker): bundle worker at build time and add it to lib

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,10 +1,2 @@
-import { broadcastLocalResponse } from './utils/broadcast';
-
-export const initWorker = () => {
-  const worker = new Worker(/* webpackChunkName: "rdk-worker" */ new URL('./worker.js', import.meta.url));
-  // broadcast worker responses to window to allow for processing multi jobs/responses in parallel
-  worker.onmessage = broadcastLocalResponse;
-  return worker;
-};
-
+export * from './instanciate';
 export * from './utils';
