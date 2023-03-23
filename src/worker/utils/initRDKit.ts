81d1c26b9c1b1945c67bc196e322bab68f52e934
Ferhat Attar <ferhat.attar@iktos.com>
2023-03-23 10:50:54 +0100

    feat: add new provider props
    Add rdkitPath and rdkitWorkerPath props to handle case where rdkit module
    and worker are not in default public path `/`. Also add initialWorkerInstance
    props to handle case where we need to init and use worker outside of react context
    
    Other changes:
      - update typings
      - update readme

--- src/worker/utils/initRDKit.ts
+++ src/worker/utils/initRDKit.ts
@@ -1,4 +1,4 @@
-﻿/* 
+﻿/*
   MIT License
 
   Copyright (c) 2023 Iktos
@@ -25,16 +25,22 @@
 import { MAX_CACHED_JSMOLS } from '../../constants';
 import { RDKitProviderCacheOptions } from '../../contexts';
 
-export const initRdkit = async ({ preferCoordgen, cache = {} }: InitWorkerOptions) => {
+export const initRdkit = async ({ rdkitPath, preferCoordgen, cache = {} }: InitWorkerOptions) => {
   if (cache) {
     initWorkerCache(cache);
   }
+
+  if (globalThis.workerRDKit) return;
+
+  const path = rdkitPath || '/RDKit_minimal.js';
+  const url = new URL(path, globalThis.origin);
   //@ts-ignore
-  importScripts(`${globalThis.origin}/RDKit_minimal.js`);
+  importScripts(url);
   if (!globalThis.initRDKitModule) return;
   await globalThis.initRDKitModule().then((rdkitModule) => {
     globalThis.workerRDKit = rdkitModule;
   });
+  //@ts-ignore
   globalThis.workerRDKit.prefer_coordgen(preferCoordgen);
 };
 
@@ -52,4 +58,5 @@ const initWorkerCache = (cache: RDKitProviderCacheOptions) => {
 interface InitWorkerOptions {
   preferCoordgen: boolean;
   cache?: RDKitProviderCacheOptions;
+  rdkitPath?: string;
 }
