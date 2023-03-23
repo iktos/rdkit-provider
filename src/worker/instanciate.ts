672962a97b5db327b754a2c6fa8e017ca83c2dae
Ferhat Attar <ferhat.attar@iktos.com>
2023-03-23 10:50:54 +0100

    feat: add new provider props
    Add rdkitPath and rdkitWorkerPath props to handle case where rdkit module
    and worker are not in default public path `/`. Also add initialWorkerInstance
    props to handle case where we need to init and use worker outside of react context
    
    Other changes:
      - update typings
      - update readme

--- src/worker/instanciate.ts
+++ src/worker/instanciate.ts
@@ -24,8 +24,9 @@
 
 import { broadcastLocalResponse } from './utils/broadcast';
 
-export const initWorker = () => {
-  const worker = new Worker(new URL(`${globalThis.origin}/rdkit-worker.js`));
+export const initWorker = (rdkitWorkerPath?: string) => {
+  const path = rdkitWorkerPath || '/rdkit-worker.js';
+  const worker = new Worker(new URL(path, globalThis.origin));
   worker.onmessage = broadcastLocalResponse;
   return worker;
 };
