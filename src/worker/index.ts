552ce29b07eadd1d8a129448cd43e356b679fb24
Ferhat Attar <ferhat.attar@iktos.com>
2023-04-13 10:30:30 +0200

    feat: add new exports
    Add to exports initWorker, postWorkerJob and RDKIT_WORKER_ACTIONS constant

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,4 +1,4 @@
-﻿/* 
+﻿/*
   MIT License
 
   Copyright (c) 2023 Iktos
@@ -24,3 +24,4 @@
 
 export * from './instanciate';
 export * from './utils';
+export { RDKIT_WORKER_ACTIONS } from './actions';
